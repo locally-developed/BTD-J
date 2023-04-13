b92c7e5884a220c5911c72ffa4e954f747a6e143
lenovo <kasper@fixitquik.ca>
2023-04-13 12:00:28 -0400

    BloonFactory

--- src/main/java/org/btdj/game/Factories/BloonFactory.java
+++ src/main/java/org/btdj/game/Factories/BloonFactory.java
@@ -1,5 +1,22 @@
 package org.btdj.game.Factories;
 
-public class BloonFactory {
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import org.btdj.game.Components.BloonsComponent;
+import org.btdj.game.MainApp;
 
+public class BloonFactory implements EntityFactory {
+    @Spawns("bloon")
+    public Entity newBloon(SpawnData data) {
+        Entity bloon = FXGL.entityBuilder(data)
+                .view("bloons/blue.png")
+                .type(MainApp.EntityType.BLOON)
+                .build();
+        bloon.addComponent(new BloonsComponent());
+
+        return bloon;
+    }
 }
