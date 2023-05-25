1e09de06dc79a37a11e377ed72952dd6921ea2f2
s201061277 <s201061277@ddsbstudent.ca>
2023-05-25 12:54:48 -0400

    added round data

--- src/main/java/org/btdj/game/Data/Bloons/Bloon.java
+++ src/main/java/org/btdj/game/Data/Bloons/Bloon.java
@@ -3,9 +3,9 @@ package org.btdj.game.Data.Bloons;
 import org.btdj.game.Components.BloonsComponent;
 
 interface Bloon {
-    BloonsComponent.Type type;
-    double speed;
-    Bloon parent;
+    BloonsComponent.Type type = BloonsComponent.Type.BLUE;
+    double speed = 0;
+    Bloon parent = this;
     Bloon child;
     int rbe;
 
