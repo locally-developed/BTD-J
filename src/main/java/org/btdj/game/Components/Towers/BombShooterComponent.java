f2ac7031dd8c9425fd6dcba146a47758bf25528a
s201061277 <s201061277@ddsbstudent.ca>
2023-06-15 12:50:05 -0400

    cleaned up imports, declared vars final

--- src/main/java/org/btdj/game/Components/Towers/BombShooterComponent.java
+++ src/main/java/org/btdj/game/Components/Towers/BombShooterComponent.java
@@ -1,6 +1,5 @@
 package org.btdj.game.Components.Towers;
 
-import com.almasb.fxgl.dev.editor.EntityInspector;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.GameWorld;
@@ -8,7 +7,6 @@ import com.almasb.fxgl.texture.AnimatedTexture;
 import com.almasb.fxgl.texture.AnimationChannel;
 import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.image.Image;
 import javafx.util.Duration;
 import org.btdj.game.Components.BloonsComponent;
 import org.btdj.game.EntityType;
@@ -20,7 +18,7 @@ public class BombShooterComponent extends TowerComponent{
     private final GameWorld world = FXGL.getGameWorld();
     private double coolDown = 1.5;
     private TargetingPriority targetingPriority = TargetingPriority.FIRST;
-    private AnimatedTexture explosion = new AnimatedTexture(new AnimationChannel(
+    private final AnimatedTexture explosion = new AnimatedTexture(new AnimationChannel(
           FXGL.getAssetLoader().loadImage("explosion.png"),
           5,
           150,
@@ -29,7 +27,7 @@ public class BombShooterComponent extends TowerComponent{
             0,
             24
     ));
-    private Entity explosionObject = FXGL.entityBuilder()
+    private final Entity explosionObject = FXGL.entityBuilder()
             .at(-150,150)
             .view(explosion)
             .buildAndAttach();
